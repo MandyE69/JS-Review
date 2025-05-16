a11e7fb843012f5ee05ccd9f0d9aade72025c01c
MandyE69 <mandipgyawali436@gmail.com>
2025-05-16 13:57:50 +0545

    ternaries instead of if/else statements

--- script.js
+++ script.js
@@ -143,41 +143,109 @@ function getBook(id) {
   return data.find((d) => d.id === id);
 }
 
-// TIPS---- use Quokka Extension in VS-code for in line output-----------------------
-
-//Destructuring 
-//   bhaneko kunai pani object acces garne choto tarika or new method to assign a objects value
+//-----------------------------destructuring-----------------------------------
+//    bhaneko kunai pani object acces garne choto tarika or new method to assign a objects value
 //  --> yo euta syntax ho jun le hamlai kunai pani array or properties of object lai unpack garna ani tesko value chai euta chutai varaible ma store garna help garcha
+
 const books = getBooks();
-books; // sabai book haru lai get garcha jun hamro data bhanne obj ma cha
+books;
 
-const book = getBook(1); 
-book; // yo variable ma chai tyo index ko matra huncha jun hamle call gareko or input diyeko cham
+const book = getBook(3);
+book;
 
-const {title, author, genres, publicationDate, pages, hasMovieAdaptation} = book;// ehha chai title author genres haru ko data chai unpack gareko 
-// console.log(title, author, genres);
+const {title, author, genres, publicationDate, pages, hasMovieAdaptation} = book;
+console.log(title, author, genres);
 
-const [firstGenre, secondGenre] = genres; //yo chai hamro genres bhako index ma vako value lai name deko 
-console.log(title, author, firstGenre, secondGenre);
+const [primary, secondary] = genres;
+primary
+secondary
 
-// rest operator
-//---- yo operator le remaining index bhako value jun ma name assign bhako chaina teslai eekai thauma assign garcha
+// ------------------------------rest operator-----------------------//
+//    yo basically kunai pani array ko remaining value lai assign garna use garincha
 
-const [firstGenres, secondGenres, ...others] = genres; // ehha others ma chai remaining value haru assign huncha
-console.log(others);
+const [primary1, secondary1, ...others] = genres;
+console.log(primary, secondary, others)
 
-// spread operator
-// ---- yo pani rest jasto hoo tara eesle chai naya value add garna help garcha array ra object ma 
+// ----------------------------------spread operator --------------------------...
+//  yo pani rest jasto hoo tara eesle chai naya value add garna help garcha array ra object ma 
 
-// spread operator for arrays
-const newGenres = [...genres, "action","romantic"] 
-console.log(newGenres); //as we can see "action", "romantic " has been added inside the generes
+// suru ma array ma herum
+const newGenres = [...genres, "action"];
+newGenres; // we can see that "action " has been added at the end index of array we can also add at the begining by place ...genres before the value that you are assinging 
 
-//spread operator for objects
-const updatedbook = book;
-updatedbook = {...book, realseDate: 3-3-2022, pages: 444 }
-updatedbook; // releasDate add bhako cha bhane pages chai update bhako cha
+// aba object ko lagi
+const updatedBook = {...book, moviePublicationDate: 2020-1-2, pages: 132} // here is for an object where we have added new properties "moviePublicationDate" and overwrite a pages 
+updatedBook;
 
-const summary = `${title} is a book written by a ${author}, it was published in ${publicationDate}`;
+//----------------------------template literals--------------------------------//
+//.... yo bhaneko kunai pani string ma hamle ${} sign use garera kunai pani expression, function haru add or call garna milcha
+
+const getYear = (str) => str.split("-")[0];
+
+const summary = `${title} is a book written by ${author} and was publised in ${getYear(publicationDate)}. The book ${hasMovieAdaptation?"":"not"} have been adopted as movie`;
 summary;
 
+//------------------ternaries instead of if else statemets----------------------//
+// yesh le chai inline condition achieve garna milcha jaha if else launa namilne huncha
+// syntax: condition? yes:no; yes/no refer to the desired string or sentence if the condition is true it will give yes else no. as you can see we have added it in summary above to illustrate it
+const pageRange = pages > 1000 ? "over a thousand" : "less than thousand";
+pageRange;
+console.log(`The pages is ${pageRange}`);
+
+//-----------------Arrow function------------------------//
+// yo chai mainly inliner function lekhna lai use gareni bhayoo, traditional bhanda different hunu bhayoo esma fucntion lekhi rakhnu parne just parameter pass garne ani arrow le denote garne ani euta variable ma store garne.
+//traditional function
+// function getYear(str){
+//   return str.split("-")[0];
+// }
+
+//----------------------arrow function--------------------//
+// const getYear = (str) => str.split("-")[0];
+console.log(getYear(publicationDate));
+
+//-------------------------- short circuiting ----------------------- //
+// for AND operator 
+
+console.log(true && "hello world")// true huda chai pachadi pani gayoo
+console.log(false && "aba dedaina") // false huda chai pachadi ko check nai nagari niklincha bhaneko matlab false 
+
+console.log(hasMovieAdaptation && "this book has a movie");
+
+//falsy: 0 , NULL, false, "", undefined
+
+console.log("" && "hello roe")
+console.log("hele" && "tptop")
+
+//for OR operator 
+// esma thakai opposite to AND operator 
+
+console.log(true || "dedaina");
+console.log(false || "dencha");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+spanishTranslation;
+
+// let see another for short circuiting which is ?? 
+
+// console.log(book.reviews.librarything.reviewsCount)
+// const wrongInput = book.reviews.librarything.reviewsCount || "NO data"; 
+// wrongInput; // here we can see when there is 0 value it return next value which is wrong because theere is some data so, for this consequence we can use ?? operator 
+
+// const whatOp = book.reviews.librarything.reviewsCount ?? "No data";
+// whatOp;
+
+// ................Optional Chaining............................................//
+// eedi kunai properties ma value chaina bhane teslai access garda js le undefined dekhai dincha tesaile lai over come garna ? sign used garincha jasle value chaina bhane ignore garcha rather the setting it undefined.. tala ko example ma herna milcha...
+
+function getTotalReview(book){
+
+  const goodreads = book.reviews?.goodreads?.reviewsCount ?? 0;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0; //last ko ?? operator use garera default 0 value halna milyoo edi NaN ayo bhane yaa tyo properties nai chaina bhane 
+  librarything;
+
+  return goodreads + librarything;
+}
+
+console.log(getTotalReview(book))
